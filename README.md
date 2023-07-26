542267ea850da3d03c8ccb224ef8e943bc28c008
YoussefShabo <youssefshabo@gmail.com>
2023-07-26 19:51:29 +0200

    July 25th

--- README.md
+++ README.md
@@ -7,5 +7,17 @@ This repo is meant to keep all of the Ruby and Ruby on Rails Practice
 - gets stands for Get String
 - no need for a semicolon at the end of the command/ line
 - # is the Comment marker
+- another way to comment is :
+  =begin
+
+      ==
+      !=
+      >
+      <
+      >=
+      <=
+
+=end
+
 - print will keep things on the same line and puts will skip to the next line
 - Uppercase is used for Class in Ruby and a variable will start with a lowercase
--- hello.rb
+++ hello.rb
@@ -1,23 +1,44 @@
 system "clear"
 
-first_name = "Youssef"
-last_name = "Shabo"
-name = first_name + " " + last_name
-puts name
+# first_name = "Youssef"
+# last_name = "Shabo"
+# name = first_name + " " + last_name
+# puts name
 
 #Getting User Input
 
-print "Enter Your Name: "
-name = gets.chomp
+# print "Enter Your Name: "
+# name = gets.chomp
 
-puts "Hello " + name 
-puts "How are you today #{ name.upcase }?"
+# puts "Hello " + name 
+# puts "How are you today #{ name.upcase }?"
 
 # MATH
 
-puts 10 + 5
-puts 10 - 5
-puts 10 * 5
-puts 10 / 5
-puts 10 ** 5
-puts 10 % 5
+# puts 10 + 5
+# puts 10 - 5
+# puts 10 * 5
+# puts 10 / 3
+# puts 10 / 3.0
+# puts 10 ** 5
+# puts 10 % 5
+
+# GETS WITH MATH
+
+# print "Enter temprature in Fahrenheit: "
+# number = gets.to_i
+
+# puts "this temprature in Celsius is equal to : #{ (number - 32) * 5/9 }"
+
+# COMAPRISON OPERATORS
+
+=begin
+
+    ==  
+    !=
+    >
+    <
+    >=
+    <=
+
+=end
